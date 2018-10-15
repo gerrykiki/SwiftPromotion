f8ac721edc2d20f3667a44ca12e57679597ee998
Gerry PW Lin <gerry_pw_lin@wistron.com>
2018-10-15 17:40:20 +0800

    Initial Commit

---
+++ Swift_Promotion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Promotion
+//
+//  Created by Po wei Lin on 2018/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
